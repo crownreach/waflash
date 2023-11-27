69e3ada2ce59fa0cd9738c6b4f2f7f46f8b5edc0
crownreach <lazuee.me@gmail.com>
2023-11-27 18:36:15 +0000

    update

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-let is_mobile = /Mobi/i.test(window.navigator.userAgent); if (is_mobile) {
-    function scrollToSubject() { try { window.scrollTo({ top: 100, left: 0, behavior: 'smooth' }); } catch (e) { } } scrollToSubject(); window.addEventListener("orientationchange",
-        function () { setTimeout(scrollToSubject, 100); });
-} else { document.getElementById('canvas').focus(); } document.getElementById('canvas').addEventListener("keydown", function (ev) { ev.preventDefault(); ev.stopPropagation(); }); document.getElementById('canvas').addEventListener("click",
-    function () { document.getElementById('canvas').focus(); }); document.addEventListener("mousedown", (function () {
-        const canvasElement = document.getElementById('canvas'); let focused = false; return function (ev) {
-            if (ev.target == canvasElement) { if (!focused) { canvasElement.focus(); focused = true; } } else { if (focused) { focused = false; } } return true;
-        }
-    })()); import { createWaflash } from 'https://cdn.jsdelivr.net/gh/crownreach/waflash@main/waflash-player.min.js';
-createWaflash(gameConfig.swfUrl, window.wafOptions || {});
+if(/Mobi/i.test(window.navigator.userAgent)){function scrollToSubject(){try{window.scrollTo({top:100,left:0,behavior:"smooth"})}catch(e){}}scrollToSubject(),window.addEventListener("orientationchange",(function(){setTimeout(scrollToSubject,100)}))}else document.getElementById("canvas").focus();document.getElementById("canvas").addEventListener("keydown",(function(e){e.preventDefault(),e.stopPropagation()})),document.getElementById("canvas").addEventListener("click",(function(){document.getElementById("canvas").focus()})),document.addEventListener("mousedown",function(){const e=document.getElementById("canvas");let t=!1;return function(n){return n.target==e?t||(e.focus(),t=!0):t&&(t=!1),!0}}());import{createWaflash as e}from"https://cdn.jsdelivr.net/gh/crownreach/waflash@main/waflash-player.min.js";e(gameConfig.swfUrl,window.wafOptions||{});
